f359882b9f1c76a28dc01d3909f10ff147051376
enazbanks <enazrbanks@gmail.com>
2023-07-07 16:08:38 +1000

    new challenge

---
+++ problem-solving/timeConversion.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+str = '07:30:00'
+date_str = str.split(':')
+print(date_str)
+#date_format = '%H:%M:%S'
+
+#date_obj = datetime.strptime(date_str, date_format)
+#print(date_obj)
